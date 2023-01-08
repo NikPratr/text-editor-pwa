76cac96c16f6449f2133e6a11519310ffac68196
Nick Prater <NikPratr@gmail.com>
2023-01-08 14:19:19 -0500

    building to heroku

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -10,22 +10,28 @@ export default class {
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
-
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
-      lineNumbers: true,
-      lineWrapping: true,
-      autofocus: true,
-      indentUnit: 2,
-      tabSize: 2,
-    });
-
+    try { 
+      this.editor = CodeMirror(document.querySelector('#main'), {
+        value: '',
+        mode: 'javascript',
+        theme: 'monokai',
+        lineNumbers: true,
+        lineWrapping: true,
+        autofocus: true,
+        indentUnit: 2,
+        tabSize: 2,
+      });
+    }
+    catch(err) {
+      console.log("Error: ", err);
+    }
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
+      console.info('Data: ', data);
+      console.info('Local data: ', localData);
+      console.info('Header: ', header);
       this.editor.setValue(data || localData || header);
     });
 
